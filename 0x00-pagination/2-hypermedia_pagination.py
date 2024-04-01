a4e7fb55d1ff69fe23d16da46fc5b0e454f86638
Neivanny1 <nevileneivanny@gmail.com>
2024-04-01 20:13:08 +0000

    get_hyper method that takes the same arguments (and defaults) as get_page and returns a dictionary containing the following key-value pairs

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -4,13 +4,14 @@ A function named index_range that takes two
 integer arguments page and page_size
 '''
 import csv
-from typing import List, Tuple
+import math
+from typing import List, Tuple, Dict
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start = (page - 1) * page_size
     end = start + page_size
-    return (start, end)
+    return start, end
 
 
 class Server:
@@ -35,12 +36,21 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
-        """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
-            return []
-        return data[start:end]
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Retrieves information about a page.
+        """
+        start, end = index_range(page, page_size)
+        page_data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        page_info = {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
+        }
+        return page_info
