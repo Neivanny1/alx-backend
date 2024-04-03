9ac0d9eb369a331e97cd0829353d945ea4ab6edf
Neivanny1 <nevileneivanny@gmail.com>
2024-04-03 10:23:19 +0000

    added functions documentations

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,7 +9,17 @@ BaseCaching = __import__('base_caching').BaseCaching
 class BasicCache(BaseCaching):
     def put(self, key, item):
         if key and item:
+            '''
+            assigns to the dictionary self.cache_data
+            the item value for the key key
+            If key or item is None, this method should not do anything
+            '''
             self.cache_data[key] = item
 
     def get(self, key):
+        '''
+        Must return the value in self.cache_data linked to key.
+        If key is None or if the key doesn`t
+        exist in self.cache_data, return None.
+        '''
         return self.cache_data.get(key)
