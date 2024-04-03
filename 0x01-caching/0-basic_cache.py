096bf12d23d05d0c57e204be04c07cc53aefe893
Neivanny1 <nevileneivanny@gmail.com>
2024-04-03 10:19:09 +0000

    fixed import issues

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,14 +3,13 @@
 Create a class BasicCache that inherits from
 BaseCaching and is a caching system
 '''
-from base_caching import BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
     def put(self, key, item):
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
-        return self.cache_data.get(key, None)
+        return self.cache_data.get(key)
