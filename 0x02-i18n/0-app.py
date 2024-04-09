03269d0ede55f7a0c2826f5e115caa96c6ebbdcb
Neivanny1 <nevileneivanny@gmail.com>
2024-04-09 22:11:47 +0000

    . Create a single / route and an index.html template that simply outputs “Welcome to Holberton” as page title (<title>) and “Hello world” as header (<h1>)

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,12 +8,13 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def get_index():
+    return render_template('0-index.html')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
